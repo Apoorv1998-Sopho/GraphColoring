607376656baa651194ad579c64b3c43cbd40d3ff
Nitiksha-Modi <nitiksha98@gmail.com>
2018-04-27 20:17:28 +0530

    added coloring

--- graph_coloring.cpp
+++ graph_coloring.cpp
@@ -6,6 +6,8 @@
 #include<stdlib.h>
 #include<map>
 #include<set>
+#include <cstdio>
+#include<algorithm>
 
 using namespace std;
 
@@ -21,6 +23,132 @@ vector<string> split(string str, string dlm){
 	}
     return arr;
 }
+
+//priority queue with change key
+class MinIndexedPQ {
+    int NMAX, N, *heap, *index, *keys;
+
+    void swap(int i, int j) {
+        int t = heap[i]; heap[i] = heap[j]; heap[j] = t;
+        index[heap[i]] = i; index[heap[j]] = j;
+    }
+
+    void bubbleUp(int k)    {
+        while(k > 1 && keys[heap[k/2]] > keys[heap[k]])   {
+            swap(k, k/2);
+            k = k/2;
+        }
+    }
+
+    void bubbleDown(int k)  {
+        int j;
+        while(2*k <= N) {
+            j = 2*k;
+            if(j < N && keys[heap[j]] > keys[heap[j+1]])
+                j++;
+            if(keys[heap[k]] <= keys[heap[j]])
+                break;
+            swap(k, j);
+            k = j;
+        }
+    }
+
+public:
+    // Create an empty MinIndexedPQ which can contain atmost NMAX elements
+    MinIndexedPQ(int NMAX)  {
+        this->NMAX = NMAX;
+        N = 0;
+        keys = new int[NMAX + 1];
+        heap = new int[NMAX + 1];
+        index = new int[NMAX + 1];
+        for(int i = 0; i <= NMAX; i++)
+            index[i] = -1;
+    }
+	
+    ~MinIndexedPQ() {
+        delete [] keys;
+        delete [] heap;
+        delete [] index;
+    }
+
+    // check if the PQ is empty
+    bool isEmpty()  {
+        return N == 0;
+    }
+
+    // check if i is an index on the PQ
+    bool contains(int i)    {
+        return index[i] != -1;
+    }
+
+    // return the number of elements in the PQ
+    int size()  {
+        return N;
+    }
+
+    // associate key with index i; 0 < i < NMAX
+    void insert(int i, int key) {
+        N++;
+        index[i] = N;
+        heap[N] = i;
+        keys[i] = key;
+        bubbleUp(N);
+    }
+
+    // returns the index associated with the minimal key
+    int minIndex()  {
+        return heap[1];
+    }
+
+    // returns the minimal key
+    int minKey()    {
+        return keys[heap[1]];
+    }
+
+    // delete the minimal key and return its associated index
+    // Warning: Don't try to read from this index after calling this function
+    int deleteMin() {
+        int min = heap[1];
+        swap(1, N--);
+        bubbleDown(1);
+        index[min] = -1;
+        heap[N+1] = -1;
+        return min;
+    }
+
+    // returns the key associated with index i
+    int keyOf(int i)    {
+        return keys[i];
+    }
+
+    // change the key associated with index i to the specified value
+    void changeKey(int i, int key)  {
+        keys[i] = key;
+        bubbleUp(index[i]);
+        bubbleDown(index[i]);
+    }
+
+    // decrease the key associated with index i to the specified value
+    void decreaseKey(int i, int key)    {
+        keys[i] = key;
+        bubbleUp(index[i]);
+    }
+
+    // increase the key associated with index i to the specified value
+    void increaseKey(int i, int key)    {
+        keys[i] = key;
+        bubbleDown(index[i]);
+    }
+
+    // delete the key associated with index i
+    void deleteKey(int i)   {
+        int ind = index[i];
+        swap(ind, N--);
+        bubbleUp(ind);
+        bubbleDown(ind);
+        index[i] = -1;
+    }
+};
 
 int main()
 {
@@ -51,6 +179,7 @@ int main()
     	}
     	
     	//add edges to the graph
+    	int degree[n_vertices+1] = {0};
     	if(flag == 1 && n_vertices != -1){
     		while(getline(inFile,line)){
     			arr = split(line, " ");
@@ -59,6 +188,8 @@ int main()
     				int v2 = atoi(arr[2].c_str());
     				graph[v1].insert(v2);
 					graph[v2].insert(v1);
+					degree[v1] += 1;
+					degree[v2] += 1;
 				}
 			}
 		}
@@ -76,7 +207,37 @@ int main()
 		
 		//start dsatur algorithm to color the graph
 		//----------------------------------------------------------------------
+		//add vertices to the priority queue
+		int saturation[n_vertices + 1] = {0};
+		int color[n_vertices + 1] = {0};
+		MinIndexedPQ pq(n_vertices);
 		
+		for(int i = 1; i <= n_vertices+1; i++)
+		{
+			pq.insert(i, -1*degree[i]);
+		}
+		while(!pq.isEmpty())
+		{
+			int current_node = pq.deleteMin();
+			int min_color = 1;
+			int color_ind[n_vertices + 1] = {0};
+			int ind;
+			for (set<int>::iterator it=graph[current_node].begin(); it!=graph[current_node].end(); ++it)
+			{
+				 color_ind[color[*it]] = 1;
+			}
+			for(int k=1; k<=n_vertices+1; k++)
+			{
+				if(color_ind[k] == 0)
+				{
+					ind = k;
+					printf("%d\n", ind);
+					break;
+				}
+			}
+			color[current_node] = ind;
+			color_ind[n_vertices + 1] = {0};
+		}
 		//----------------------------------------------------------------------
 		return 0;
 	}
